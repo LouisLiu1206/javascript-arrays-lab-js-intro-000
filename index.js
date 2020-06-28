0b56749daa60900cc527d7d7093762014428f1a2
Qize <louisliu1206@yahoo.com>
2020-06-28 02:32:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function destructivelyAppendKitten(name) {
  kittens.push(name)
   return kittens;
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens;
+}
